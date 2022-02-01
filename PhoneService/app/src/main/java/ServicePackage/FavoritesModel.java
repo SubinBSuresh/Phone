e9563376dd66b422b645a2c1a7244d02754bc38b
SubinBSuresh <subin.bsuresh27@gmail.com>
2022-02-01 18:14:23 +0530

    Added Database Methods and Contact SuggestionAdapter (no working RecyclerView)

---
+++ PhoneService/app/src/main/java/ServicePackage/FavoritesModel.java
@@ -0,0 +1,72 @@
+package ServicePackage;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class FavoritesModel implements Parcelable {
+    private int id;
+    private String name, number;
+
+    public FavoritesModel() {
+    }
+
+    public FavoritesModel(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    protected FavoritesModel(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        number = in.readString();
+    }
+
+    public static final Creator<FavoritesModel> CREATOR = new Creator<FavoritesModel>() {
+        @Override
+        public FavoritesModel createFromParcel(Parcel in) {
+            return new FavoritesModel(in);
+        }
+
+        @Override
+        public FavoritesModel[] newArray(int size) {
+            return new FavoritesModel[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        dest.writeString(number);
+    }
+}
