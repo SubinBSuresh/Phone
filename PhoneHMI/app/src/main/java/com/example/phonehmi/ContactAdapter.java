f26b951e69a978c8a3be1248816a51298cbdf937
SubinBSuresh <subin.bsuresh27@gmail.com>
2022-02-15 13:54:28 +0530

    Revert "edited files by nihala"
    This reverts commit 2ac5c0a88b83488be09133cba20f67c12fe69f80.

--- PhoneHMI/app/src/main/java/com/example/phonehmi/ContactAdapter.java
+++ PhoneHMI/app/src/main/java/com/example/phonehmi/ContactAdapter.java
@@ -1,39 +1,26 @@
 package com.example.phonehmi;
 
 import android.content.Context;
-import android.os.RemoteException;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import ServicePackage.ContactModel;
+import java.util.concurrent.Callable;
 
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder>{
 
-    private static List<ServicePackage.ContactModel> contactList;
-
-    ContactModel contactModel;
+    ArrayList<ContactModel> ContactHolder;
     private Context context;
 
-
-             //   }
-          //  });
-      }
-
-
-    public ContactAdapter(List<ContactModel> contactList, Context context) {
-        this.contactList = contactList;
+    public ContactAdapter(ArrayList<ContactModel> ContactHolder, Context context) {
+        this.ContactHolder = ContactHolder;
         this.context = context;
     }
 
@@ -48,99 +35,34 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        holder.name.setText(contactList.get(position).getName());
-        contactModel = contactList.get(position);
+        holder.name.setText(ContactHolder.get(position).getName());
 
-
-        //subine ithu correct anonn areelatto
-       /* try {
-
-            if (MainActivity.getAidl().checkContactPresentInFavoritesTable(int id));
-            {
-                holder.arbutton.setBackgroundResource(R.drawable.ic_baseline_unstar_24);
-            }
-            else {
-                holder.arbutton.setBackgroundResource(R.drawable.ic_baseline_star_24);
-            }
-        } catch (Exception e) {
-            Log.e("%%%%%%%%%%%%", "broken");
-        }
-    }
-*/
     }
 
-        @Override
+    @Override
     public int getItemCount() {
-        return contactList.size();
+        return ContactHolder.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView name;
         ToggleButton arbutton;
 
-            //arbutton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                //@Override
-              //  public void onCheckedChanged(CompoundButton compoundButton, boolean ischecked) {
-/*
-                    if (ischecked) {
-                        arbutton.setBackgroundResource(R.drawable.ic_baseline_star_24);
-                        try {
-                            MainActivity.getAidl().checkContactPresentInFavoritesTable(contactList.get(getAdapterPosition()));
-                            MainActivity.getAidl().addContactToFavorites(contactList.get(getAdapterPosition()));
-                        } catch (RemoteException e) {
-                            e.printStackTrace();
-                        }
-                    }else {
-                      arbutton.setBackgroundResource(R.drawable.ic_baseline_unstar_24);
-                        try {
-                            MainActivity.getAidl().removeContactFromFavorites.(contactList.get(getAdapterPosition()));
-                        } catch (RemoteException e) {
-                            e.printStackTrace();
-                        }
-            };*/
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-           name = itemView.findViewById(R.id.contact_name);
-           arbutton = itemView.findViewById(R.id.btn_add_favorite);
+            name = itemView.findViewById(R.id.contact_name);
+            arbutton= itemView.findViewById(R.id.btn_add_favorite);
 
-            //sheriyano enn doubt aanu
-
-            //arbutton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                //@Override
-              //  public void onCheckedChanged(CompoundButton compoundButton, boolean ischecked) {
-/*
-                    if (ischecked) {
-                        arbutton.setBackgroundResource(R.drawable.ic_baseline_star_24);
-                        try {
-                            MainActivity.getAidl().checkContactPresentInFavoritesTable(contactList.get(getAdapterPosition()));
-                            MainActivity.getAidl().addContactToFavorites(contactList.get(getAdapterPosition()));
-                        } catch (RemoteException e) {
-                            e.printStackTrace();
-                        }
-                    }else {
-                      arbutton.setBackgroundResource(R.drawable.ic_baseline_unstar_24);
-                        try {
-                            MainActivity.getAidl().removeContactFromFavorites.(contactList.get(getAdapterPosition()));
-                        } catch (RemoteException e) {
-                            e.printStackTrace();
-                        }
-            };*/
-
-}
-             //   }
-          //  });
-      }
+            arbutton.setBackgroundDrawable(ContextCompat.getDrawable(context, android.R.drawable.star_big_off));
 
 
-        @Override
-        public void onClick(View view) {
-            TextView name  = view.findViewById(R.id.contact_name);
-            Toast.makeText(view.getContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
-            Log.e("*******************", name.getText().toString());
         }
-    }
 
+
+
+    }
 }
+
+
--- PhoneHMI/app/src/main/java/com/example/phonehmi/ContactFragment.java
+++ PhoneHMI/app/src/main/java/com/example/phonehmi/ContactFragment.java
@@ -1,41 +1,74 @@
 package com.example.phonehmi;
 
-import android.content.ContentResolver;
-import android.database.Cursor;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.RemoteException;
-import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import java.util.ArrayList;
 
-import java.util.List;
-
-import ServicePackage.ContactModel;
-
+import ServicePackage.aidlInterface;
 
 public class ContactFragment extends Fragment {
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     RecyclerView recyclerView; //recyclerview object
+    ArrayList<ContactModel> ContactHolder;
     TextView textView;
-    List<ContactModel> contactList;
-    private ContactAdapter contactAdapter;
-    SwipeRefreshLayout swipeRefreshLayoutContacts;
-    private ContactModel contactModel;
-
+    private ContactAdapter ContactAdapter;
 
 
     public ContactFragment() {
         // Required empty public constructor
     }
-    
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ContactFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContactFragment newInstance(String param1, String param2) {
+        ContactFragment fragment = new ContactFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,47 +79,54 @@ public class ContactFragment extends Fragment {
 
         recyclerView = view.findViewById(R.id.rvView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-       // textView = view.findViewById(R.id.empty_view_contacts);
-        swipeRefreshLayoutContacts = view.findViewById(R.id.swipeRefreshLayoutContacts);
-
-        //ADDING CONTACTS FROM CONTENT PROVIDER TO CURSOR
-        ContentResolver resolver = getContext().getContentResolver();
-        Cursor cursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
-
-        // dbHelper.saveContact(cursor);
+        ContactHolder = new ArrayList<>();
+        textView = view.findViewById(R.id.empty_view_contacts);
 
 
-        swipeRefreshLayoutContacts.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        ContactModel ob1 = new ContactModel("Niha");
+        ContactHolder.add(ob1);
+        ContactModel ob2 = new ContactModel("Safa");
+        ContactHolder.add(ob2);
+        ContactModel ob3 = new ContactModel("Sreerag Quest");
+        ContactHolder.add(ob3);
+        ContactModel ob4= new ContactModel("Tineesha Quest");
+        ContactHolder.add(ob4);
+        ContactModel ob5 = new ContactModel("Sachin");
+        ContactHolder.add(ob5);
+        ContactModel ob6 = new ContactModel("Subin");
+        ContactHolder.add(ob6);
+        ContactModel ob7 = new ContactModel("Arya");
+        ContactHolder.add(ob7);
+        ContactModel ob8 = new ContactModel("Pappa");
+        ContactHolder.add(ob8);
+        ContactModel ob9 = new ContactModel("Mom");
+        ContactHolder.add(ob9);
+        ContactModel ob10 = new ContactModel("Dheedi");
+        ContactHolder.add(ob10);
+        ContactModel ob11 = new ContactModel("Subin1");
+        ContactHolder.add(ob11);
+        ContactModel ob12 = new ContactModel("Arya1");
+        ContactHolder.add(ob12);
+        ContactModel ob13 = new ContactModel("Simi");
+        ContactHolder.add(ob13);
+        ContactModel ob14 = new ContactModel("Mamma");
+        ContactHolder.add(ob14);
+        ContactModel ob15 = new ContactModel("Bhayya");
+        ContactHolder.add(ob15);
 
+        ContactAdapter  = new ContactAdapter(ContactHolder, getContext());
+        recyclerView.setAdapter(ContactAdapter);
+        //display no contact msg
+        updateVisibility();
 
-            @Override
-            public void onRefresh() {
-
-                swipeRefreshLayoutContacts.setRefreshing(false);
-                recyclerView.setAdapter(contactAdapter);
-                contactAdapter.notifyDataSetChanged();
-                contactAdapter = new ContactAdapter( contactList, getContext());
-
-            }
-        });
-        contactAdapter = new ContactAdapter(contactList, getContext());
-        recyclerView.setAdapter(contactAdapter);
-        contactAdapter.notifyDataSetChanged();
-       // updateVisibility();
         return view;
+
+
+
     }
 
-    private List<ContactModel> refreshContacts() {
-        try {
-            contactList = MainActivity.getAidl().getContacts();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        return contactList;
-    }
-
-   /* private void updateVisibility() {
-        if (contactAdapter.getItemCount() == 0) {
+    private void updateVisibility() {
+        if(ContactAdapter.getItemCount()==0){
             recyclerView.setVisibility(View.GONE);
             textView.setVisibility(View.VISIBLE);
         } else {
@@ -94,7 +134,4 @@ public class ContactFragment extends Fragment {
             textView.setVisibility(View.GONE);
         }
     }
-*/
-
 }
-
