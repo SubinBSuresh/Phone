0765874f0121be37a0fe420831d388ec43966d5b
SubinBSuresh <subin.bsuresh27@gmail.com>
2022-02-15 14:19:45 +0530

    latest file

--- PhoneHMI/app/src/main/java/com/example/phonehmi/ContactFragment.java
+++ PhoneHMI/app/src/main/java/com/example/phonehmi/ContactFragment.java
@@ -24,6 +24,7 @@ import ServicePackage.ContactModel;
 public class ContactFragment extends Fragment {
 
 
+
     RecyclerView recyclerView; //recyclerview object
     TextView textView;
     List<ContactModel> contactList;
