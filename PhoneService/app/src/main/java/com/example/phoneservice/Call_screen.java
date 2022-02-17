6a68d86b35e677703112f6c38809a1a1b8eed899
SubinBSuresh <subin.bsuresh27@gmail.com>
2022-02-17 14:40:38 +0530

    minor changes and code cleanups

--- PhoneService/app/src/main/java/com/example/phoneservice/Call_screen.java
+++ PhoneService/app/src/main/java/com/example/phoneservice/Call_screen.java
@@ -1,19 +1,21 @@
 package com.example.phoneservice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Call_screen extends AppCompatActivity {
+    //    private TextView textViewName;
+    Timer timer;
     private Chronometer chronometer;
     private boolean running;
-//    private TextView textViewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,7 @@ public class Call_screen extends AppCompatActivity {
         setContentView(R.layout.activity_call_screen);
         ImageButton btnEnd = findViewById(R.id.btnEnd);
         chronometer = findViewById(R.id.tvTimer);
-
+        timer = new Timer();
 
         if (!running) {
             chronometer.start();
@@ -29,9 +31,15 @@ public class Call_screen extends AppCompatActivity {
         }
         btnEnd.setOnClickListener(v -> {
             chronometer.stop();
-            Toast.makeText(getApplicationContext(),"call ended",Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(getApplicationContext(),Call_End.class);
-            startActivity(intent);
+            Toast.makeText(getApplicationContext(), "call ended", Toast.LENGTH_LONG).show();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), Call_End.class);
+                    startActivity(intent);
+                }
+            }, 4000);
+
 
         });
     }
