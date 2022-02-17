1ab9c9f53df0796f3bb01d0d92088cda19b2c8bb
Sreerag99 <sreeragvv99@gmail.com>
2022-02-17 15:57:47 +0530

    Sreerag new back button added

--- PhoneHMI/app/src/main/java/com/example/phonehmi/Call_end.java
+++ PhoneHMI/app/src/main/java/com/example/phonehmi/Call_end.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class Call_end extends AppCompatActivity {
@@ -13,15 +14,19 @@ public class Call_end extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_end);
         ImageButton imgeCall = findViewById(R.id.imgecall);
+        ImageButton imgeBack = findViewById(R.id.imgeback);
         imgeCall.setOnClickListener(v -> {
             Intent intent = new Intent(getApplicationContext(), Calling_Screen.class);
             startActivity(intent);
         });
+        imgeBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),DialerFragment.class);
+                startActivity(intent);
+            }
+        });
     }
-        @Override
-        public void onBackPressed() {
-            Intent intent = new Intent(getApplicationContext(), DialerFragment.class);
-            startActivity(intent);
-    //super.onBackPressed();
-    }
+
+
 }
